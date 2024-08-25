3adcbb7a30df0cf9b82bb77d7509adfc0cbf0805
dearaujofelipe <mgdefelipe@outlook.com>
2024-08-25 17:10:48 -0300

    Folder Structure + Hashtag List feature

--- src/components/feedback/FeedbackForm.tsx
+++ src/components/feedback/FeedbackForm.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { MAX_CHARACTHERS } from '../lib/constants';
+import { MAX_CHARACTHERS } from '../../lib/constants';
 
 type FeedbackFormProps = {
   onAddToList: (text: string) => void;
